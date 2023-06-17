f8b7ff895c547b6a91c756b055e77da4c53f234e
Miller1999 <millerjavierarias@gmail.com>
2023-06-16 22:36:09 -0500

    Integrado el menu en la version mobile

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const navEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
+const iconoHam = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
 
 navEmail.addEventListener("click",toggleDesktopMenu)
+iconoHam.addEventListener("click",toggleMobileMenu)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive")
+}
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive")
 }
